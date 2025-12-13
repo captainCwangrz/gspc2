5b2f90ae432d8f02dbf6b8849c2fa3b633ddffb8
Captain Nocap <18501397628@163.com>
2025-12-13 12:59:30 +0800

    Update graph.js

--- public/js/graph.js
+++ public/js/graph.js
@@ -281,7 +281,7 @@ export function createGraph({ state, config, element, onNodeClick, onLinkClick, 
     const camera = controls && controls.object ? controls.object : null;
     if (camera && renderer) {
         flyControls = new FlyControls(camera, renderer.domElement);
-        flyControls.movementSpeed = 400;
+        flyControls.movementSpeed = 350;
         flyControls.rollSpeed = Math.PI / 3;
         flyControls.dragToLook = true;
     }
